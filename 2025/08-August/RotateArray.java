beb870eb542ade5b925da250f4b1b29125733d1f
Abhinavkrishna-b <abhinavkrishna.b.2005@gmail.com>
2025-08-14 22:09:57 +0530

    RotateArray 14-08-25

---
+++ 2025/08-August/RotateArray.java
@@ -0,0 +1,39 @@
+//14-08-25
+import java.util.*;
+class RotateArraySolution{    static void rotateArr(int arr[], int k) {
+        int n = arr.length;
+        k = k % n; 
+        reverse(arr, 0, k - 1);
+        reverse(arr, k, n - 1);
+        reverse(arr, 0, n - 1);
+    }
+
+    static void reverse(int arr[], int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
+public class RotateArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of array: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter the values: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.print("Enter the number of rotations: ");
+        int k = sc.nextInt();
+        RotateArraySolution.rotateArr(arr, k);
+        System.out.println("Array after rotation: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}//Time- O(n) Space- O(1)
