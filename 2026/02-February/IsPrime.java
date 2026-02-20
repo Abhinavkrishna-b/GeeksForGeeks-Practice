dccb868b273143d70824371f6e2f44f03f510a75
Abhinavkrishna-b <abhinavkrishna.b.2005@gmail.com>
2026-02-20 21:19:26 +0530

    IsPrime 20-02-2026

---
+++ 2026/02-February/IsPrime.java
@@ -0,0 +1,29 @@
+//20-02-26
+import java.util.*;
+
+public class IsPrime {
+    static boolean isPrime(int n) {
+        if (n <= 1) return false;
+        if (n == 2) return true;
+        if (n % 2 == 0) return false;
+        for (int i = 3; i * i <= n; i += 2) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: "); 
+        int n = sc.nextInt();
+        if (isPrime(n)) {
+            System.out.println(n + " is a prime number.");
+        } else {
+            System.out.println(n + " is not a prime number.");
+        }
+
+        sc.close();
+    }
+}
+//Time- O(sqrt(n))
+//Space- O(1)
